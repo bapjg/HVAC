0af72127a1c23dbf29a0f1a4a01a6414379008ca
ADavis <ADavis@AD_Office>
2013-08-25 17:56:31 +0200

    Testing

--- eReg/eRegulation/Push_Message_Classes.bat
+++ eReg/eRegulation/Push_Message_Classes.bat
@@ -3,4 +3,8 @@ CD      \HVAC_Repository\git\HVAC\eReg\eRegulation
 COPY    Message_*.class                           D:\HVAC_Project\HVAC_Server\bin\eRegulation
 COPY    Message_*.class                           \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes\eRegulation
 
+CD 		D:\HVAC_Project\HVAC_Server\bin
+COPY    Calendar.class                            \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes
+COPY    Monitor.class                             \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes
+
 PAUSE
