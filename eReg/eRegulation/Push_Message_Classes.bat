1130c68b0897b28e13c6e6f104d519b502e7fee0
Brigitte <Brigitte@BC_Home>
2013-09-06 18:01:50 +0200

    bat

--- eReg/eRegulation/Push_Message_Classes.bat
+++ eReg/eRegulation/Push_Message_Classes.bat
@@ -1,14 +1,15 @@
 REM     This is in                                D: \HVAC_Repository\git\HVAC\eReg\eRegulation
 
 D:
-CD      \HVAC_Repository\git\HVAC\eReg\eRegulation
-COPY    Message_*.class                           D:\HVAC_Project\HVAC_Server\bin\eRegulation
-COPY    Message_*.class                           \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes\eRegulation
-
-CD 		D:\HVAC_Project\HVAC_Server\bin
-COPY    Calendar.class                            \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes
-COPY    Monitor.class                             \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes
+CD      \HVAC_Repository\HVAC\eReg\eRegulation
+REM COPY    Message_*.class                           D:\HVAC_Project\HVAC_Server\bin\eRegulation
+COPY     Message_*.class                             \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes\eRegulation
 
 PAUSE
 
-CD		\HVAC_Repository\git\HVAC\eReg\eRegulation
+CD      \HVAC_Repository\HVAC\HVAC_Server\build\classes
+COPY    *.class                                     \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes
+
+PAUSE
+
+CD		\HVAC_Repository\HVAC\eReg\eRegulation
