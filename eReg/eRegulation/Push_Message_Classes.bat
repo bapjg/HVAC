94f1b5d5482c08367bd1623b306afae646c07764
ADavis <ADavis@BC_Home>
2013-09-11 22:39:45 +0200

    Push.bat

--- eReg/eRegulation/Push_Message_Classes.bat
+++ eReg/eRegulation/Push_Message_Classes.bat
@@ -2,14 +2,13 @@ REM     This is in                                D: \HVAC_Repository\git\HVAC\e
 
 PROMPT  -
 D:
-CD      \HVAC_Repository\HVAC\eReg\eRegulation
-REM COPY    Message_*.class                           D:\HVAC_Project\HVAC_Server\bin\eRegulation
-COPY     Message_*.class                             \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes\eRegulation
 
+CD      \HVAC_Repository\HVAC\eReg\eRegulation
+COPY     Message_*.class                             \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes\eRegulation
 
 CD      \HVAC_Repository\HVAC\HVAC_Server\build\classes
 COPY    *.class                                     \\NAS\Users_AD\2_Projects\HVAC_Server\hw\WEB-INF\classes
 
 PAUSE
 
-CD		\HVAC_Repository\HVAC\eReg\eRegulation
+CD      \HVAC_Repository\HVAC\eReg\eRegulation
