976d21e78bdfba1fd0dae46baacf671941d5f30b
ADavis <ADavis@BC_Home>
2013-09-19 01:21:44 +0200

    sdfdf

--- eReg/eRegulation/Thermometer_Stabliser.java
+++ eReg/eRegulation/Thermometer_Stabliser.java
@@ -8,11 +8,13 @@ public class Thermometer_Stabliser
 	public Integer			index;
 	public Integer			depth;
 	public Integer			tolerance;
+	public Integer			count;
 
 	public Thermometer_Stabliser(Integer readingDepth, Integer tolerance)
 	{
 		this.depth 		  	  			= readingDepth;
 		this.index 		   			 	= 0;
+		this.count						= 0;
 		this.tolerance					= tolerance;
 		this.readings					= new Integer[readingDepth];
 	}
@@ -22,11 +24,12 @@ public class Thermometer_Stabliser
 		System.out.println(readings.length);
 		System.out.println("In add again");
 		
-		if (readings.length == 0)
+		if (this.count == 0)
 		{
 			System.out.println("In add == 0");
 			readings[index] 			= newReading;
 			index++;
+			count++;
 			System.out.println("Return add read 0");
 			return newReading;
 		}
@@ -44,6 +47,10 @@ public class Thermometer_Stabliser
 			{
 				readings[index] 		= newReading;
 				index					= index + 1 % depth;
+				if (count < depth)
+				{
+					count++;
+				}
 				System.out.println("Return add index : " + index);
 				System.out.println("Return add result : " + newReading);
 				return newReading;
@@ -54,10 +61,10 @@ public class Thermometer_Stabliser
 	{
 		Integer i;
 		Integer sum						= 0;
-		for (i = 0; i < readings.length; i++)
+		for (i = 0; i < this.count - 1; i++)
 		{
 			sum							+= readings[i];
 		}
-		return sum / readings.length;
+		return sum / this.count;
 	}
 }
