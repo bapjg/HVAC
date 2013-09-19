ea6266cde544273f3bb5012999708ec941100ced
ADavis <ADavis@AD_Office>
2013-09-19 16:11:30 +0200

    vbnvbn

--- HVAC_Client/HVAC_Client/src/com/bapjg/hvac_client/Mgmt_Msg_Calendar_Req.java
+++ HVAC_Client/HVAC_Client/src/com/bapjg/hvac_client/Mgmt_Msg_Calendar_Req.java
@@ -1,11 +1,11 @@
 package com.bapjg.hvac_client;
 
-public class Mgmt_Msg_Calendar_Request_Index extends Mgmt_Msg_Abstract
+public class Mgmt_Msg_Calendar_Req extends Mgmt_Msg_Abstract
 {
 	private static final long 		serialVersionUID 			= 1005L;
 	public String					dateTime;
 	
-	public Mgmt_Msg_Calendar_Request_Index()
+	public Mgmt_Msg_Calendar_Req()
 	{
 	}
 }
