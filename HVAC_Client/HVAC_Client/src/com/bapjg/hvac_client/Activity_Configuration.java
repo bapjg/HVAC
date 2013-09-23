88c9f574780c9cbed25f8a1b4dd3076e4ecbc5cc
ADavis <ADavis@AD_Office>
2013-09-23 22:08:47 +0200

    ConfAct

---
+++ HVAC_Client/HVAC_Client/src/com/bapjg/hvac_client/Activity_Configuration.java
@@ -0,0 +1,30 @@
+package com.bapjg.hvac_client;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class Activity_Configuration extends Activity 
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_configuration);
+		
+		System.out.println(Global.henry);
+		TextView field	 						= (TextView) findViewById(R.id.Data);
+		field.setText(Global.henry);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.configuration, menu);
+		return true;
+	}
+
+}
