fd862f64dd748860bb4a771e9552b7d99a92e1b6
a.davis <a.davis@PC-Davis.opinel.local>
2013-09-30 15:46:51 +0200

    Mgmt_Msg_Abstract Ack/Nack added

--- HVAC_Client/HVAC_Client/src/com/bapjg/hvac_client/Mgmt_Msg_Abstract.java
+++ HVAC_Client/HVAC_Client/src/com/bapjg/hvac_client/Mgmt_Msg_Abstract.java
@@ -4,7 +4,11 @@ public abstract class Mgmt_Msg_Abstract implements java.io.Serializable
 {
 	public static final long 		serialVersionUID 			= 1001L;
 	
-	public Mgmt_Msg_Abstract()
+	public class Ack extends Mgmt_Msg_Abstract
 	{
-	}	
+	}
+	public class Nack extends Mgmt_Msg_Abstract
+	{
+	}
+
 }
