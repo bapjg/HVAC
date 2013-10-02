ce0dafcae99dc65ff2f3f4c9817920621e6db273
ADavis <ADavis@AD_Office>
2013-10-03 01:08:59 +0200

    Choices

---
+++ HVAC_Client/HVAC_Client/src/Choices_Configuration.java
@@ -0,0 +1,18 @@
+package com.bapjg.hvac_client;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Choices_Configuration extends Fragment 
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.choices_configuration, container, false);
+    }
+}
+
