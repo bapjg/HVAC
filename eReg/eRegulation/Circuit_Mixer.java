b29ffba6d8cbc5d61992b723d624ab64f2ae5e75
Brigitte <Brigitte@BC_Home>
2013-10-16 14:23:20 +0200

    sdf

--- eReg/eRegulation/Circuit_Mixer.java
+++ eReg/eRegulation/Circuit_Mixer.java
@@ -81,7 +81,7 @@ public class Circuit_Mixer extends Circuit_Abstract
 //				LogIt.info("Circuit", "sequencerFloor", "Stopping Stopping zzzzzzzzzz");
 
 				if 	(	(Global.circuits.isSingleActiveCircuit())
-				&& 		(Global.thermoBoiler.reading > taskActive.tempObjective)   )
+				&& 		(Global.thermoBoiler.reading > taskActive.tempObjective + 30)   )	// Continue while more than 3 degrees
 				{
 					// We are alone, so as long as there is heat to get out of the system
 					// carry on
