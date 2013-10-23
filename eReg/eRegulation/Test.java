be00e0beb24b6aab69ed81814458987648c8cf61
ADavis <ADavis@AD_Office>
2013-10-23 21:55:56 +0200

    dfg

--- eReg/eRegulation/Test.java
+++ eReg/eRegulation/Test.java
@@ -13,7 +13,7 @@ public class Test
 		
 		Thermometer_New thermo						= new Thermometer_New("Test", "28.72D1 8504 0000", "Test friend");
 
-		thermo.read(12, false);
+		thermo.read(12, true);
 
 		Integer i;
 		for (i = 0; i < 10; i++)
