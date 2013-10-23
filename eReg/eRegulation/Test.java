0240ff983125018f74a439b7c5bd71b4fec200ef
ADavis <ADavis@AD_Office>
2013-10-23 22:09:53 +0200

    dfgdfg

--- eReg/eRegulation/Test.java
+++ eReg/eRegulation/Test.java
@@ -11,24 +11,34 @@ public class Test
 	{
 		Control 		Me 							= new Control();
 		
-		Thermometer_New thermo						= new Thermometer_New("Test", "28.72D1 8504 0000", "Test friend");
+		Thermometer_New thermo1						= new Thermometer_New("Test1", "28.72D1 8504 0000", "Test friend");
+		Thermometer_New thermo2						= new Thermometer_New("Test2", "28.9ED0 8504 0000", "Test friend");
+		Thermometer_New thermo3						= new Thermometer_New("Test3", "28.B491 8504 0000", "Test friend");
 
 		Integer i;
-		Integer temp;
+		Integer temp1, temp2, temp3;
 		for (i = 0; i < 10; i++)
 		{
 			Long now									= Global.getTimeNowSinceMidnight();
-			temp										= thermo.read();
+			temp1										= thermo1.read();
+			temp2										= thermo2.read();
+			temp3										= thermo3.read();
 			Long later									= Global.getTimeNowSinceMidnight();
-			System.out.println(i.toString() + " temp : " + temp + ", time required Norm : " + (later - now));
+			System.out.println(i.toString() + " temp : " + temp1 + ", time required Norm : " + (later - now));
+			System.out.println(i.toString() + " temp : " + temp2 + ", time required Norm : " + (later - now));
+			System.out.println(i.toString() + " temp : " + temp3 + ", time required Norm : " + (later - now));
 		}
 
 		for (i = 0; i < 10; i++)
 		{
 			Long now									= Global.getTimeNowSinceMidnight();
-			temp										= thermo.readUnCached();
+			temp1										= thermo1.readUnCached();
+			temp2										= thermo2.readUnCached();
+			temp3										= thermo3.readUnCached();
 			Long later									= Global.getTimeNowSinceMidnight();
-			System.out.println(i.toString() + " temp : " + temp + ", time required UnCa : " + (later - now));
+			System.out.println(i.toString() + " temp : " + temp1 + ", time required UnCa : " + (later - now));
+			System.out.println(i.toString() + " temp : " + temp2 + ", time required UnCa : " + (later - now));
+			System.out.println(i.toString() + " temp : " + temp3 + ", time required UnCa : " + (later - now));
 		}
 System.out.println("Done");
 	}
