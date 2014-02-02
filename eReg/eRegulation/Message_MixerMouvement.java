20831742325deba6f07462a85fdc4ef1f10efa04
ADavis <ADavis@AD_Office.home>
2014-02-02 18:39:09 +0100

    dsff

---
+++ eReg/eRegulation/Message_MixerMouvement.java
@@ -0,0 +1,10 @@
+package eRegulation;
+
+public class Message_MixerMouvement extends Message_Abstract
+{
+	private static final long 		serialVersionUID 			= 1L;
+	public Long						dateTimeStart;
+	public Integer 					positionTrackedStart;
+	public Long						dateTimeEnd;
+	public Integer 					positionTrackedEnd;
+}
