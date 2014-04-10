55670cae79b6220fcdffef211e55a013f56bca1d
AD <AD@BC_Home.bapjg.local>
2014-04-10 23:37:52 +0200

    ghgf

--- HVAC_Messages/src/HVAC_Messages/Ctrl_Immediate.java
+++ HVAC_Messages/src/HVAC_Messages/Ctrl_Immediate.java
@@ -4,6 +4,8 @@ package HVAC_Messages;
 public class Ctrl_Immediate extends Ctrl_Abstract
 {
 	private static final long 			serialVersionUID 			= 1L;
+	public 	static final int				ACTION_Start 				= 1;
+	public 	static final int				ACTION_Stop 				= 2;
 
 	public Ctrl_Immediate()
 	{
@@ -20,15 +22,19 @@ public class Ctrl_Immediate extends Ctrl_Abstract
 		public Boolean					executionActive;
 		public Integer 					tempObjective;
 		public Long 					timeStart;
+		public Long 					timeEnd;
+		public Boolean 					stopOnObjective;
 	}
 	public class Execute extends Ctrl_Immediate
 	{
-		private static final long 		serialVersionUID 			= 1L;
 		public String 					circuitName;
+		public Integer					action;
+		public Boolean					executionPlanned;
+		public Boolean					executionActive;
 		public Integer 					tempObjective;
+		public Long 					timeStart;
 		public Long 					timeEnd;
 		public Boolean 					stopOnObjective;
-		public Boolean 					start;
 	}
 	public class Ack extends Ctrl_Immediate
 	{
