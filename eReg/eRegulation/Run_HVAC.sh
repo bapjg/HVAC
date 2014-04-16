e864085ee2d3fc60cfef29bb8802f6b82d5640f9
AD <AD@BC_Home.bapjg.local>
2014-04-16 08:34:19 +0200

    Run

--- eReg/eRegulation/Run_HVAC.sh
+++ eReg/eRegulation/Run_HVAC.sh
@@ -1,13 +1,18 @@
 cd /home/pi/HVAC/eReg/eRegulation/
-git stash save
-git stash drop
-update
-
-
 
 STATUS=1
 while [ $STATUS -eq 1 ]
 do
+	git stash save
+	git stash drop
+	git pull
+	
+	/home/pi/HVAC/eReg/_Documents/update.sh
+
     sudo java -cp "../:../../HVAC_Messages/bin/:javax.mail.jar" -Djava.library.path=./            eRegulation.Control
     STATUS=$?
 done
+
+if [ $STATUS -eq 2 ];then
+	shutdown -r now
+fi
