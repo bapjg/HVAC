9ca86cae32e50288ffb0433877d6e799c6dcb80a
AD <AD@BC_Home.bapjg.local>
2014-04-18 08:19:48 +0200

    ignoregit

--- HVAC_Messages/src/HVAC_Messages/Ctrl_Parameters.java
+++ HVAC_Messages/src/HVAC_Messages/Ctrl_Parameters.java
@@ -61,6 +61,7 @@ public class Ctrl_Parameters 			extends 					Ctrl_Abstract
 		public String 					pump;
 		public String 					thermometer;
 		public Integer 					type;
+		public Integer					tempMax;
 	}
 	public class Burner 				extends  					Ctrl_Parameters
 	{
@@ -75,5 +76,26 @@ public class Ctrl_Parameters 			extends 					Ctrl_Abstract
 		public String 					thermometer;
 		public Integer					tempNeverExceed;
 		public Integer					tempOverShoot;
+	}
+	public class Gradient 				extends  					Ctrl_Parameters
+	{
+		private static final long 		serialVersionUID 			= 1L;
+		public String 					name;
+		public Integer 					outsideLow;
+		public Integer					outsideHigh;
+		public Integer 					tempLow;
+		public Integer					tempHigh;
+	}
+	public class PID					extends  					Ctrl_Parameters
+	{
+		public String					name;
+		public Integer					depth;
+		public Integer					frequency;
+	}
+	public class Mixer					extends  					Ctrl_Parameters
+	{
+		public String					name;
+		public Long						swingTime;
+		public Long						lagTime;
 	}
 }
