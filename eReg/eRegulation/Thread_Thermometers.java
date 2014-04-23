905aa790c179abeb7e09e6cbc420bea9e5c435d2
AD <AD@BC_Home.bapjg.local>
2014-04-23 17:21:55 +0200

    dfsdfsfds

--- eReg/eRegulation/Thread_Thermometers.java
+++ eReg/eRegulation/Thread_Thermometers.java
@@ -12,6 +12,7 @@ public class Thread_Thermometers implements Runnable
 			{
 				if (thisThermometer.pidControler != null)
 				{
+					System.out.println("thisThermo " + thisThermometer.name);
 					if (thisThermometer.pidControler.sampleIncrement == 1)
 					{
 						System.out.println("ThermoName YES = " + thisThermometer.name);
