5726b3222433fb451ff1da830d63367e6e4b00fa
AD <AD@BC_Home.bapjg.local>
2014-04-23 16:24:15 +0200

    sdqsdq

--- eReg/eRegulation/Global.java
+++ eReg/eRegulation/Global.java
@@ -158,6 +158,7 @@ public class Global extends DefaultHandler
 		{
 			System.out.println(dateTimeDisplay() + " Thermo added " + configurationDetail.name);
 			System.out.println(dateTimeDisplay() + " Thermo added ad" + configurationDetail.address);
+			System.out.println(dateTimeDisplay() + " xxx" );
 			System.out.println(dateTimeDisplay() + " Thermo added pid" + configurationDetail.pidName);
 			Global.thermometers.addFromObject(configurationDetail.name, configurationDetail.address, configurationDetail.pidName);
 		}
