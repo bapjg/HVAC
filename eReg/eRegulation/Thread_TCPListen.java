f4a8ada679f156849b51f3d66b0ff85ec274019e
AD <AD@BC_Home.bapjg.local>
2014-04-25 00:06:08 +0200

    ytr

--- eReg/eRegulation/Thread_TCPListen.java
+++ eReg/eRegulation/Thread_TCPListen.java
@@ -293,9 +293,9 @@ public class Thread_TCPListen 			implements Runnable
 	{
 		Ctrl_Actions_Relays.Data message_return				= new Ctrl_Actions_Relays().new Data();
 		message_return.burner 								= Global.burnerPower.isOn();
-		message_return.pumpHotWater	 						= Global.pumpWater.relay.isOn();
-		message_return.pumpFloor	 						= Global.pumpFloor.relay.isOn();
-		message_return.pumpRadiator	 						= Global.pumpRadiator.relay.isOn();
+		message_return.pumpHotWater	 						= Global.pumps.fetchPump("Hot_Water").relay.isOn();
+		message_return.pumpFloor	 						= Global.pumps.fetchPump("Floor").relay.isOn();
+		message_return.pumpRadiator	 						= Global.pumps.fetchPump("Radiator").relay.isOn();
         return message_return;
 	}
 	private Ctrl_Actions_Relays.Data 	process_Ctrl_Actions_Relays_Execute		(Ctrl_Actions_Relays.Execute message_in)
@@ -311,15 +311,15 @@ public class Thread_TCPListen 			implements Runnable
 		}
 		else if (message_in.relayName.equalsIgnoreCase("HotWater"))
 		{
-			relay											= Global.pumpWater.relay;
+			relay											= Global.pumps.fetchPump("Hot_Water").relay;
 		}
 		else if (message_in.relayName.equalsIgnoreCase("Floor"))
 		{
-			relay											= Global.pumpFloor.relay;
+			relay											= Global.pumps.fetchPump("Floor").relay;
 		}
 		else if (message_in.relayName.equalsIgnoreCase("Radiator"))
 		{
-			relay											= Global.pumpRadiator.relay;
+			relay											= Global.pumps.fetchPump("Radiator").relay;
 		}
 		if (relay != null)
 		{
