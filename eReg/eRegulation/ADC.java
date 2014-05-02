89805673c9dd96cff8f50164f170a19815065e88
AD <AD@BC_Home.bapjg.local>
2014-05-02 23:02:28 +0200

    sss

--- eReg/eRegulation/ADC.java
+++ eReg/eRegulation/ADC.java
@@ -8,7 +8,7 @@ public class ADC
 	private native int 		ReadAverage();
 	
 	public  ADC()
-	{int x = 1;
+	{
 		int ADC_Channels			= 2;		// Analog input is second channel. Need to monitor 2 channels
 		int ADC_Samples				= 64;		// Number of samples in a read (Default value) (64 = 0x40 ie need to remove bits 0x3F)
 		int ADC_Bits_To_Shift		= 6;		// Shifting 6 bits is dividing by 64
