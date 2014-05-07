1ec27114dd584be852c6739850997aea8aefa2cb
AD <AD@BC_Home.bapjg.local>
2014-05-07 12:19:09 +0200

    Manage weatherdata error

--- HVAC_Messages/src/HVAC_Messages/Ctrl_WeatherData.java
+++ HVAC_Messages/src/HVAC_Messages/Ctrl_WeatherData.java
@@ -133,6 +133,8 @@ public class Ctrl_WeatherData 						extends 		DefaultHandler
 		catch (Exception e) 
 		{
 			System.out.println("Ctrl_WeatherData/Constructor error on contacting the weather server");
+			this.dateTimeObtained													= null;
+			this.forecasts															= null;
 			return;	// Will try again in 5 mins (loop timer)
 		}
 		//
