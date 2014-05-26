d68abfcbe241011ad7c6093a080f49e884aa34db
AD <AD@BC_Home.bapjg.local>
2014-05-26 13:24:40 +0200

    Common/bin

--- eReg/eRegulation/Run_HVAC.sh
+++ eReg/eRegulation/Run_HVAC.sh
@@ -9,7 +9,7 @@ do
 	
 	/home/pi/HVAC/eReg/_Documents/update.sh
 
-    sudo java -cp "../:../../HVAC_Messages/bin/:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
+    sudo java -cp "../:../../HVAC_Common/bin/:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
     STATUS=$?
 done
 
