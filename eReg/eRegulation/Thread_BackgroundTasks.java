ef9d6b9f97bfa7a06750d2073c66c1b10a87c4ed
AD <AD@BC_Home.bapjg.local>
2014-06-03 00:46:18 +0200

    jgh

--- eReg/eRegulation/Thread_BackgroundTasks.java
+++ eReg/eRegulation/Thread_BackgroundTasks.java
@@ -163,8 +163,9 @@ public class Thread_BackgroundTasks implements Runnable
 			        		LogIt.info("Thread_Background", "Run", "Weather item:"+time_from+"-"+time_to+" temp:"+ forecastItem.temperature.value.intValue()+" clds: "+forecastItem.clouds.all.toString(), true);
 			         		LogIt.info("Thread_Background", "Run", "Summer temp:"+ ((Integer) Global.tasksBackGround.summerTemp/1000), true);
 			         		LogIt.info("Thread_Background", "Run", "sunshineInfluence:"+ ((Integer) Global.tasksBackGround.sunshineInfluence/1000), true);
+			         		LogIt.info("Thread_Background", "Run", "cloudInfluence:"+ ((Integer) (100 - forecastItem.clouds.all)), true);
 						    
-			         		Integer efectiveTemp = forecastItem.temperature.value.intValue() + Global.tasksBackGround.sunshineInfluence * forecastItem.clouds.all/100;
+			         		Integer efectiveTemp = forecastItem.temperature.value.intValue() + Global.tasksBackGround.sunshineInfluence * (100 - forecastItem.clouds.all)/100;
 			        		LogIt.info("Thread_Background", "Run", "Effective temp:" + efectiveTemp, true);
 			        	}
 			        }
