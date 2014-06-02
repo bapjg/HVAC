9ada5c60c0b77d7cd027d73e6bafc87b1a864693
AD <AD@BC_Home.bapjg.local>
2014-06-03 00:02:29 +0200

    dsfdfs

--- HVAC_Common/src/HVAC_Common/Ctrl_WeatherData.java
+++ HVAC_Common/src/HVAC_Common/Ctrl_WeatherData.java
@@ -234,6 +234,7 @@ public class Ctrl_WeatherData 						extends 		DefaultHandler
 		}
 		catch (Exception e) 
 		{
+			e.printStackTrace();
 			System.out.println("Ctrl_WeatherData/Constructor error parsing the xml");
 			this.dateTimeObtained													= null;
 			this.forecasts															= null;
