055fa7b253b0660e86ee99be423dc0e211f747f7
Andre <Andre@BC_Home.home>
2014-10-29 12:15:38 +0100

    sdf

--- eReg/eRegulation/Run_HVAC.sh
+++ eReg/eRegulation/Run_HVAC.sh
@@ -1,5 +1,8 @@
 cd /home/pi/HVAC/eReg/eRegulation/
 
+sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 12:04
+sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 13:08
+
 STATUS=1
 while [ $STATUS -eq 1 ]
 do
@@ -9,7 +12,6 @@ do
 	
 	/home/pi/HVAC/eReg/_Documents/update.sh
 
-#    sudo java -cp "../:HVAC_Common.jar:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
     sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
     STATUS=$?
 done
