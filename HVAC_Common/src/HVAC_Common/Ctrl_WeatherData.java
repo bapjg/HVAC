c23d283f26c3d825892365c4be53f5c0f5f20429
Andre <Andre@BC_Home.home>
2014-11-23 22:55:56 +0100

    sfdsfd

--- HVAC_Common/src/HVAC_Common/Ctrl_WeatherData.java
+++ HVAC_Common/src/HVAC_Common/Ctrl_WeatherData.java
@@ -136,7 +136,7 @@ public class Ctrl_WeatherData 						extends 		DefaultHandler
 		} 
 		catch (SocketTimeoutException eTO) 
 		{
-			System.out.println("Ctrl_WeatherData/Constructor timeout contacting the weather server" + eTO.toString());
+			System.out.println("Ctrl_WeatherData/Constructor timeout contacting the weather server" + eTO.toString() + ", will try later");
 			this.dateTimeObtained													= null;
 			this.forecasts															= null;
 			return;	// Will try again in 5 mins (loop timer)
