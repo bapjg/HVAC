018110bfb249cd3f7108fb0fcf95b1428984cc5f
Andre <Andre@BC_Home.home>
2014-12-18 08:16:38 +0100

    aez

--- eReg/eRegulation/Mixer.java
+++ eReg/eRegulation/Mixer.java
@@ -109,8 +109,8 @@ public class Mixer
 		
 		swingTimeRequired																	= pidFloorOut.getGain(gainP, gainD, gainI); 					// returns a swingTime in milliseconds
 		
-		// Take into consideration 2nd order changes
-		swingTimeRequired																	= swingTimeRequired - Math.round(gainP * gainD * pidFloorOut.d2Tdt2());
+		// Take into consideration 2nd order changes (dis = s + ut + at²/s)
+		swingTimeRequired																	= swingTimeRequired - Math.round(gainP * gainD * gainD * pidFloorOut.d2Tdt2())/2;
 		
 		if (tempFloorOut > 50000)
 		{
