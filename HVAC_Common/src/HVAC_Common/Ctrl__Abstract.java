c79612f7cad25815cb860565148cee2b540f74c7
Andre <Andre@192.168.5.102>
2015-08-18 01:43:09 +0200

    zrezrezre

--- HVAC_Common/src/HVAC_Common/Ctrl__Abstract.java
+++ HVAC_Common/src/HVAC_Common/Ctrl__Abstract.java
@@ -8,6 +8,7 @@ package HVAC_Common;
 
 public class Ctrl__Abstract implements java.io.Serializable
 {
+	private int x =3;
 	private static final long 		serialVersionUID 	= 1L;
 	
 	@SuppressWarnings("serial")
