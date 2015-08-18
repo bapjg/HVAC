deacd8c3d1bc92848cced5bcc3bd5ba7e39f78b0
Andre <Andre@192.168.5.102>
2015-08-19 00:10:23 +0200

    Probes implemented before testing

--- eReg/eRegulation/Thermometer.java
+++ eReg/eRegulation/Thermometer.java
@@ -17,7 +17,7 @@ import HVAC_Common.Ctrl_Configuration;
 public class Thermometer
 {
 	public String 												name;
-	public String 												friendlyName;
+//	public String 												friendlyName;
 	public String 												address;
 	public String 												thermoFile_Normal;
 	public String 												thermoFile_UnCached;
@@ -28,7 +28,7 @@ public class Thermometer
 	public Thermometer(Ctrl_Configuration.Thermometer 			paramThermometer)
 	{
 		this.name 		    																= paramThermometer.name;
-		this.friendlyName  																	= "";
+//		this.friendlyName  																	= "";
 		this.address  																		= paramThermometer.address;
 		this.pidControler																	= null;
 		
@@ -77,41 +77,55 @@ public class Thermometer
      	 *  	A change of resolution (either up or down) seems to be uncached
      	 */
     	
-		String	 		tempString;
-		float	 		tempFloat;
-    	
-    	try
+//		String	 		tempString;
+//		float	 		tempFloat;
+//    	
+//    	try
+//		{
+//    		FileInputStream 								ThermoFile_InputStream 		= null;
+//    		if (unCached)
+//    		{
+//        		ThermoFile_InputStream 													= new FileInputStream(thermoFile_UnCached + "temperature" + resolution.toString());
+//    		}
+//    		else
+//    		{
+//        		ThermoFile_InputStream 													= new FileInputStream(thermoFile_Normal   + "temperature" + resolution.toString());
+//    		}
+//			DataInputStream 								ThermoFile_InputData 		= new DataInputStream(ThermoFile_InputStream);
+//			BufferedReader 									ThermoFile_InputBuffer 		= new BufferedReader(new InputStreamReader(ThermoFile_InputData));
+//			String 											ThermoFile_InputLine 		= ThermoFile_InputBuffer.readLine();
+//
+//			ThermoFile_InputBuffer.close();
+//			ThermoFile_InputData.close();
+//			ThermoFile_InputStream.close();
+//
+//			tempString	 																= ThermoFile_InputLine.replace(" ", "");
+//			tempFloat	 																= Float.parseFloat(tempString);
+//			this.reading																= Math.round(tempFloat * 1000); // Round to milli-degree
+//		}
+//		catch (Exception err)
+//		{
+//			if (!this.name.equalsIgnoreCase("Boiler_In"))
+//			{
+//				System.out.println("Thermometer read Error on " + this.name + " message was : " + err.getMessage());
+//			}
+//			this.reading																= -273000; // Round to milli-degree
+//		}		
+//		return this.reading; //Last known good reading;
+		
+		Integer												readings					= 0;
+		Integer												count						= 0;
+		for (Probe probe : probes)
 		{
-    		FileInputStream 								ThermoFile_InputStream 		= null;
-    		if (unCached)
-    		{
-        		ThermoFile_InputStream 													= new FileInputStream(thermoFile_UnCached + "temperature" + resolution.toString());
-    		}
-    		else
-    		{
-        		ThermoFile_InputStream 													= new FileInputStream(thermoFile_Normal   + "temperature" + resolution.toString());
-    		}
-			DataInputStream 								ThermoFile_InputData 		= new DataInputStream(ThermoFile_InputStream);
-			BufferedReader 									ThermoFile_InputBuffer 		= new BufferedReader(new InputStreamReader(ThermoFile_InputData));
-			String 											ThermoFile_InputLine 		= ThermoFile_InputBuffer.readLine();
-
-			ThermoFile_InputBuffer.close();
-			ThermoFile_InputData.close();
-			ThermoFile_InputStream.close();
-
-			tempString	 																= ThermoFile_InputLine.replace(" ", "");
-			tempFloat	 																= Float.parseFloat(tempString);
-			this.reading																= Math.round(tempFloat * 1000); // Round to milli-degree
-		}
-		catch (Exception err)
-		{
-			if (!this.name.equalsIgnoreCase("Boiler_In"))
+			Integer											aReading					= probe.read(resolution, unCached);
+			if (aReading != null)
 			{
-				System.out.println("Thermometer read Error on " + this.name + " message was : " + err.getMessage());
+				readings																+= aReading;
+				count++;
 			}
-			this.reading																= -273000; // Round to milli-degree
-		}		
-		return this.reading; //Last known good reading;
+		}
+		this.reading																	= readings / count;
+		return this.reading;
 	}
     public String toDisplay()
     {
@@ -121,11 +135,66 @@ public class Thermometer
     }
     public class Probe
     {
+    	public String 										name;
     	public String 										address;
+    	public String 										thermoFile_Normal;
+    	public String 										thermoFile_UnCached;
+    	public Boolean										probeOk;
     	
     	public Probe(Ctrl_Configuration.Thermometer 		paramThermometer)
     	{
+    		this.name																	= paramThermometer.name;
     		this.address																= paramThermometer.address;
+    		this.probeOk																= true;
+    		String prefix																= "/mnt/1wire/";
+    		String suffix																= "/";
+
+    		this.thermoFile_Normal														= prefix               + address.toUpperCase().replace(" ", "") + suffix; // remove spaces from address like '28-0000 49ec xxxx'
+    		this.thermoFile_UnCached													= prefix + "uncached/" + address.toUpperCase().replace(" ", "") + suffix; // remove spaces from address like '28-0000 49ec xxxx'
     	}
+ 	    public Integer read(Integer resolution, Boolean unCached)
+		{
+	     	// Returns temperature in millidegrees
+	     	/*
+	     	 *  Read times are :
+	     	 *  	When in cache : 10 ms
+	     	 *  	Not in cache  : 9 bit  - 300 ms
+	     	 *  	Not in cache  : 10 bit - 400 ms
+	     	 *  	Not in cache  : 11 bit - 500 ms
+	     	 *  	Not in cache  : 12 bit - 900 ms
+	     	 *  
+	     	 *  	A change of resolution (either up or down) seems to be uncached
+	     	 */
+	    	
+			String	 		tempString;
+			float	 		tempFloat;
+	    	
+	    	try
+			{
+	    		FileInputStream 								ThermoFile_InputStream 		= null;
+	    		if (unCached)	        						ThermoFile_InputStream 		= new FileInputStream(thermoFile_UnCached + "temperature" + resolution.toString());
+	    		else							        		ThermoFile_InputStream 		= new FileInputStream(thermoFile_Normal   + "temperature" + resolution.toString());
+
+				DataInputStream 								ThermoFile_InputData 		= new DataInputStream(ThermoFile_InputStream);
+				BufferedReader 									ThermoFile_InputBuffer 		= new BufferedReader(new InputStreamReader(ThermoFile_InputData));
+				String 											ThermoFile_InputLine 		= ThermoFile_InputBuffer.readLine();
+
+				ThermoFile_InputBuffer.close();
+				ThermoFile_InputData.close();
+				ThermoFile_InputStream.close();
+
+				tempString	 																= ThermoFile_InputLine.replace(" ", "");
+				tempFloat	 																= Float.parseFloat(tempString);
+				if (! probeOk)									LogIt.display("Probe", "read", "Thermometer now reading Ok on " + this.name + "-" + this.address);
+				probeOk																		= true;
+				return	Math.round(tempFloat * 1000); // Round to milli-degree
+			}
+			catch (Exception err)
+			{
+				if (probeOk)									LogIt.display("Probe", "read", "Thermometer read Error on " + this.name + "-" + this.address + " message was : " + err.getMessage());
+				probeOk																		= false;
+				return null;
+			}		
+		}
     }
 }
