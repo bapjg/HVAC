1d4c692b860d0ad3906f0a88a1ff48ed549beb28
Andre <Andre@192.168.5.102>
2015-10-18 18:19:00 +0200

    dcffdcf

--- eReg/eRegulation/Circuit_Abstract.java
+++ eReg/eRegulation/Circuit_Abstract.java
@@ -187,7 +187,7 @@ abstract class Circuit_Abstract
 				LogIt.error("Circuit_Abstract", "taskActivate", "A task is active when it shouldn't be");
 				LogIt.info("Circuit_Abstract", "taskActivate", "Task to activate is occupied... Replaced");
 				LogIt.info("Circuit_Abstract", "taskActivate", "Task to activate is already active");
-				LogIt.display("Circuit_Abstract", "taskActivate", "taskActive          = " + System.identityHashCode(taskActive));
+				LogIt.display("Circuit_Abstract", "taskActivate", "taskActive          = " + taskActive.days + " +++ " + taskActive.timeStartDisplay);
 				LogIt.display("Circuit_Abstract", "taskActivate", "thisTask(candidate) = " + System.identityHashCode(thisTask));
 			}
 			this.taskActive																	= thisTask;
