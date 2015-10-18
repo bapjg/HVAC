26f2e8688462db88a1d9fdac54f7c336c5853255
Andre <Andre@192.168.5.102>
2015-10-18 22:34:08 +0200

    sfd

--- HVAC_Common/src/HVAC_Common/Ctrl_WeatherData.java
+++ HVAC_Common/src/HVAC_Common/Ctrl_WeatherData.java
@@ -117,7 +117,7 @@ public class Ctrl_WeatherData 						extends 		DefaultHandler
 		
 		try 
 		{
-			URL 									serverURL 						= new URL("http://api.openweathermap.org/data/2.5/forecast/city?q=chambery,fr&mode=xml&units=metric&type=accurate");
+			URL 									serverURL 						= new URL("http://api.openweathermap.org/data/2.5/forecast/city?q=chambery,fr&mode=xml&units=metric&type=accurate&appid=41515f9a266234d303e26f056173c60b");
 			HttpURLConnection  						serverConnection 				= (HttpURLConnection) serverURL.openConnection();
 			serverConnection.setRequestMethod("GET");
 			serverConnection.setConnectTimeout(5000);
