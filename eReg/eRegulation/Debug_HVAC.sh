ff5fe14fb1da516112484a46e03af834490b05ba
Andre <Andre@192.168.5.102>
2015-10-24 15:38:05 +0200

    gfd

--- eReg/eRegulation/Debug_HVAC.sh
+++ eReg/eRegulation/Debug_HVAC.sh
@@ -11,22 +11,11 @@ git pull
 	
 /home/pi/HVAC/eReg/_Documents/update.sh
 
-#sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" \
-#-Dcom.sun.management.jmxremote                                          \
-#-Dcom.sun.management.jmxremote.port=5005                                \
-#-Dcom.sun.management.jmxremote.authenticate=false                       \
-#-Dcom.sun.management.jmxremote.ssl=false                                \
-#-Djava.rmi.server.hostname=raspberrypi                                  \
-#-Djava.library.path=./            eRegulation.Control
-
 sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" \
--Xdebug                                                                 \
--Xrunjdwp:transport=dt_socket                                           \
-                                                              \
-                                                              \
 -Dcom.sun.management.jmxremote                                          \
 -Dcom.sun.management.jmxremote.port=5005                                \
 -Dcom.sun.management.jmxremote.authenticate=false                       \
 -Dcom.sun.management.jmxremote.ssl=false                                \
--Djava.rmi.server.hostname=pi                                           \
+-Djava.rmi.server.hostname=raspberrypi                                  \
 -Djava.library.path=./            eRegulation.Control
+
