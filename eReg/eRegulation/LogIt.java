685b14149da79d400412cd86d45fe7d72ca42033
Andre <Andre@192.168.5.102>
2015-11-10 13:13:18 +0100

    hkj

--- eReg/eRegulation/LogIt.java
+++ eReg/eRegulation/LogIt.java
@@ -31,7 +31,7 @@ public class LogIt
 	{
 		try
 		{
-	        FileWriter fw = new FileWriter("//NAS/DropZone/HVAC_FogFile.txt",true); //the true will append the new data
+	        FileWriter fw = new FileWriter("/mnt/DZ/HVAC_FogFile.txt",true); //the true will append the new data
 	        fw.write(message + "\n");//appends the string to the file
 	        fw.flush();
 	        fw.close();
