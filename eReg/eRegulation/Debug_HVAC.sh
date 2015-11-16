9ef242060c86bea51e1ee5e5a0a7cb0162ef61a1
Andre <Andre@192.168.5.102>
2015-11-16 23:30:46 +0100

    dgf

--- eReg/eRegulation/Debug_HVAC.sh
+++ eReg/eRegulation/Debug_HVAC.sh
@@ -13,7 +13,7 @@ git pull
 
 sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" \
 -Xdebug -Xnoagent                                                       \
--Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005           \
+-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005           \
 -Djava.library.path=./            eRegulation.Control
 
 
