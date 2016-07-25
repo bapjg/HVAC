73b08e70dbf84ef0c6f74290c81a5a7378c889d6
Andre <Andre@bapjg.com>
2016-07-26 00:22:25 +0200

    Run_HVAC

--- eReg/eRegulation/Run_HVAC.sh
+++ eReg/eRegulation/Run_HVAC.sh
@@ -16,32 +16,38 @@ do
 	then
     	sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
     	STATUS=$?
+    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Now rebooting
     fi
     
     if [ $STATUS -eq 6 ];	# Debug Restart Wait
     then
 		sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005  -Djava.library.path=./  eRegulation.Control
     	STATUS=$?
+    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Now Debug/Wait
 	fi
 	
 	if [ $STATUS -eq 7 ];	# Debug Restart No Wait
 	then
 		sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005  -Djava.library.path=./  eRegulation.Control
     	STATUS=$?
+   		sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Now Debug/NoWait
 	fi
 done
 
 if [ $STATUS -eq 2 ];	# Reboot
 then
+	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Rebooting
 	sudo shutdown -r now
 fi
 
 if [ $STATUS -eq 5 ];	# Shutdown
 then
+	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Halting
 	sudo shutdown -h now
 fi
 
 if [ $STATUS -eq 0 ];	# GoTo Bash
 then
+	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Going to Bash
 	echo Going to bash
 fi
