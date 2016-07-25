cf00bfa16c287c3daa750ae377295b84c8b06de5
Andre <Andre@bapjg.com>
2016-07-26 00:51:49 +0200

    Run_HVAC

--- eReg/eRegulation/Run_HVAC.sh
+++ eReg/eRegulation/Run_HVAC.sh
@@ -12,44 +12,60 @@ do
 	
 	/home/pi/HVAC/eReg/_Documents/update.sh
 
-	if [ $STATUS -eq 1 ];	# Normal Restart
+	if [ $STATUS -eq 1 ];	# Normal Restart application
 	then
     	sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
     	STATUS=$?
-    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:00
+    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:40			# Position Line 2 (ie 2 x 32 Hex) (0 to 3)
     	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit.
-    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t Now restarting
+   		sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:60			# Position Line 3 (ie 3 x 32 Hex) (0 to 3)
+     	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t Now restarting
     fi
     
     if [ $STATUS -eq 6 ];	# Debug Restart Wait
     then
 		sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005  -Djava.library.path=./  eRegulation.Control
     	STATUS=$?
-    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Now Debug/Wait
+    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:40
+    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit.
+   		sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:60
+     	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t Now Debug/Wait
 	fi
 	
 	if [ $STATUS -eq 7 ];	# Debug Restart No Wait
 	then
 		sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005  -Djava.library.path=./  eRegulation.Control
     	STATUS=$?
-   		sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Now Debug/NoWait
+    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:40
+    	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit.
+   		sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:60
+     	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t Now Debug/NoWait
 	fi
 done
 
 if [ $STATUS -eq 2 ];	# Reboot
 then
-	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Rebooting
+    sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:40
+    sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit.
+   	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:60
+     sudo bw_tool -I -D /dev/i2c-1 -a 94 -t Now Rebooting
 	sudo shutdown -r now
 fi
 
 if [ $STATUS -eq 5 ];	# Shutdown
 then
-	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Halting
+   	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:40
+  	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit.
+	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:60
+   	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t Now Halting
 	sudo shutdown -h now
 fi
 
 if [ $STATUS -eq 0 ];	# GoTo Bash
 then
-	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit. Going to Bash
+   	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:40
+   	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t HVAC Quit.
+	sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 11:60
+   	sudo bw_tool -I -D /dev/i2c-1 -a 94 -t Going to Bash
 	echo Going to bash
 fi
