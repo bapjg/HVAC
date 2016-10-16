8494d874775f5159fdbb11f7fab4966fcb2cb116
Andre <Andre@bapjg.com>
2016-10-16 12:16:12 +0200

    xvc

--- eReg/eRegulation/Scan.c
+++ eReg/eRegulation/Scan.c
@@ -10,7 +10,7 @@
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 #include <linux/i2c-dev.h>
-#include <./Interfaces.h>
+#include "./Interfaces.h"
 
 //----------------------------------------------------------
 //
