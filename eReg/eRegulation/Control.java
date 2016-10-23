e6520ded942a149fca4b1a6c3ec33b4fc829a4f3
Andre <Andre@bapjg.com>
2016-10-23 23:50:24 +0200

    xvc

--- eReg/eRegulation/Control.java
+++ eReg/eRegulation/Control.java
@@ -40,7 +40,7 @@ public class Control
 		
 //		if (System.getProperty("os.name").equalsIgnoreCase("windows"))	System.out.println("libraries not loaded");
 //		else															System.loadLibrary("Interfaces");
-		System.load("/home/pi/HVAC/eReg/eRegulation/libInterfaces.so");
+		System.load("home/pi/HVAC/eReg/eRegulation/libInterfaces.so");
 		
 //	    System.setProperty("jna.library.path","/home/pi/HVAC/eReg/eRegulation/");
 
