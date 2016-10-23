9a2e5334c7f69cac0649006dbd976eb5e1cd8d3a
Andre <Andre@bapjg.com>
2016-10-23 10:36:35 +0200

    fgdfdgfdg

--- eReg/eRegulation/Run_HVAC.sh
+++ eReg/eRegulation/Run_HVAC.sh
@@ -3,8 +3,6 @@ cd /home/pi/HVAC/eReg/eRegulation/
 sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 12:04
 sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 13:08
 
-exit
-
 STATUS=1
 while (( STATUS != 0 && STATUS != 2 && STATUS != 5));
 do
