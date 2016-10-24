050bd5e6820601f2e99965bd7931355f51608979
Andre <Andre@bapjg.com>
2016-10-24 23:45:30 +0200

    fds

--- eReg/eRegulation/Interfaces_I2C.c
+++ eReg/eRegulation/Interfaces_I2C.c
@@ -252,7 +252,7 @@ JNIEXPORT void JNICALL Java_eRegulation_LCD_BlinkOn		(JNIEnv *env, jobject obj)	
 JNIEXPORT void JNICALL Java_eRegulation_LCD_BlinkOff	(JNIEnv *env, jobject obj)							{	LCD_BlinkOff();				}
 JNIEXPORT void JNICALL Java_eRegulation_LCD_Write		(JNIEnv *env, jobject obj, jstring Message)
 {
-	char *nativestring =(*env)->GetStringUTFChars(env, Message, 0);
+	const char *nativestring =(*env)->GetStringUTFChars(env, Message, 0);
 	LCD_Write(nativestring);
 	(*env)->ReleaseStringUTFChars(env, Message, nativestring);
 }
