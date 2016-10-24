c4c70403c4851d3e9f7d960dcfb1eb138392f38b
Andre <Andre@bapjg.com>
2016-10-24 23:09:10 +0200

    dsf

--- eReg/eRegulation/Control.java
+++ eReg/eRegulation/Control.java
@@ -39,7 +39,7 @@ public class Control
 		//
 		
 		if (System.getProperty("os.name").equalsIgnoreCase("windows"))	System.out.println("libraries not loaded");
-		else															System.loadLibrary("Interfaces");
+		else															System.loadLibrary("Interfaces_SPI");
 //		System.load("/home/pi/HVAC/eReg/eRegulation/libInterfaces.so");
 		
 //	    System.setProperty("jna.library.path","/home/pi/HVAC/eReg/eRegulation/");
