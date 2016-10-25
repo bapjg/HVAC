76b0fa093a55ca0e27476e7e495ff7c2513493f5
Andre <Andre@bapjg.com>
2016-10-25 12:26:30 +0200

    fdg

--- eReg/eRegulation/Interfaces_SPI.c
+++ eReg/eRegulation/Interfaces_SPI.c
@@ -191,7 +191,7 @@ static void scanAndSet()
 	
 	char *found;
 	found 						= strstr(buf, "spi_big");
-printf("0x%02X : ",spi_port);
+printf("0x%02X : ", spi_port);
 printout(buf);
 //	if (found == NULL)
 	if (found != NULL)
@@ -204,6 +204,7 @@ printout(buf);
 			buf[0] 				= port | 1;						// OR 1 means return status info
 			buf[1] 				= 1;							// Ident
 			spi_txrx(buf, 0x2, 0x20);
+printf("0x%02X : ", port);
 printout(buf);
 			found = strstr(buf, "spi_big");
 			if (found != NULL)
