5849c892bc9927cd338b1f23580b18971d4adb98
Andre <Andre@bapjg.com>
2016-12-27 14:54:38 +0100

    No Display all lines commented out

--- eReg/eRegulation/LCD.java
+++ eReg/eRegulation/LCD.java
@@ -17,52 +17,59 @@ public class LCD
 
 	public void clear()
 	{
-		Global.interfaceSemaphore.semaphoreLock("LDC.clear");
-		Clear();
-		Global.interfaceSemaphore.semaphoreUnLock();
+		return;
+//		Global.interfaceSemaphore.semaphoreLock("LDC.clear");
+//		Clear();
+//		Global.interfaceSemaphore.semaphoreUnLock();
 	}
 	public void write(String sText)
 	{
-		Global.interfaceSemaphore.semaphoreLock("LDC.write");
-		displayText = sText;
-		Write(displayText);
-		Global.interfaceSemaphore.semaphoreUnLock();
+		return;
+//		Global.interfaceSemaphore.semaphoreLock("LDC.write");
+//		displayText = sText;
+//		Write(displayText);
+//		Global.interfaceSemaphore.semaphoreUnLock();
 	}
 	public void writeAtPosition(int iLine, int iColumn, String sText)
 	{
-		Global.interfaceSemaphore.semaphoreLock("LDC.writeAtPosition");
-		Position(iLine, iColumn);
-		Global.waitMilliSeconds(50);
-		displayText = sText;
-		Write(displayText);
-		Global.interfaceSemaphore.semaphoreUnLock();
+		return;
+//		Global.interfaceSemaphore.semaphoreLock("LDC.writeAtPosition");
+//		Position(iLine, iColumn);
+//		Global.waitMilliSeconds(50);
+//		displayText = sText;
+//		Write(displayText);
+//		Global.interfaceSemaphore.semaphoreUnLock();
 	}
 	public void blinkAtPosition(int iLine, int iColumn)
 	{
-		Global.interfaceSemaphore.semaphoreLock("LDC.blinkAtPosition");
-		Position(iLine, iColumn);
-		Global.waitMilliSeconds(50);
-		BlinkOn();
-		Global.interfaceSemaphore.semaphoreUnLock();
+		return;
+//		Global.interfaceSemaphore.semaphoreLock("LDC.blinkAtPosition");
+//		Position(iLine, iColumn);
+//		Global.waitMilliSeconds(50);
+//		BlinkOn();
+//		Global.interfaceSemaphore.semaphoreUnLock();
 	}
 	public void position(int iLine, int iColumn)
 	{
-		Global.interfaceSemaphore.semaphoreLock("LDC.position");
-		Position(iLine, iColumn);
-		// I've found that screen gets screwed up
-		//global.wait(1);
-		Global.interfaceSemaphore.semaphoreUnLock();
+		return;
+//		Global.interfaceSemaphore.semaphoreLock("LDC.position");
+//		Position(iLine, iColumn);
+//		// I've found that screen gets screwed up
+//		//global.wait(1);
+//		Global.interfaceSemaphore.semaphoreUnLock();
 	}
 	public void blinkOn()
 	{
-		Global.interfaceSemaphore.semaphoreLock("LDC.blinkOn");
-		BlinkOn();
-		Global.interfaceSemaphore.semaphoreUnLock();
+		return;
+//		Global.interfaceSemaphore.semaphoreLock("LDC.blinkOn");
+//		BlinkOn();
+//		Global.interfaceSemaphore.semaphoreUnLock();
 	}
 	public void blinkOff()
 	{
-		Global.interfaceSemaphore.semaphoreLock("LDC.blinkOff");
-		BlinkOff();
-		Global.interfaceSemaphore.semaphoreUnLock();
+		return;
+//		Global.interfaceSemaphore.semaphoreLock("LDC.blinkOff");
+//		BlinkOff();
+//		Global.interfaceSemaphore.semaphoreUnLock();
 	}
 }
