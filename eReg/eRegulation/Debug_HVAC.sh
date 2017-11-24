13f686dcea0854395f89945454ae3e2bf3788126
Andre <Andre@bapjg.com>
2017-11-24 16:52:10 +0100

    hgfhgf

--- eReg/eRegulation/Debug_HVAC.sh
+++ eReg/eRegulation/Debug_HVAC.sh
@@ -1,13 +1,13 @@
 cd /home/pi/HVAC/eReg/eRegulation/
 
-sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 12:04
-sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 13:08
+# sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 12:04
+# sudo bw_tool -I -D /dev/i2c-1 -a 94 -w 13:08
 
 STATUS=1
 
-#git stash save
-#git stash drop
-#git pull
+git stash save
+git stash drop
+git pull
 	
 #/home/pi/HVAC/eReg/_Documents/update.sh
 
