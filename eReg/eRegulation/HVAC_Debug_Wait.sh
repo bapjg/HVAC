9c8cab4ea7e77c0d7caf9e7ed53f04f6bfbff92f
Andre <Andre@AD_Bureau>
2017-11-27 10:03:19 +0100

    rr

--- eReg/eRegulation/HVAC_Debug_Wait.sh
+++ eReg/eRegulation/HVAC_Debug_Wait.sh
@@ -6,4 +6,4 @@ git pull
 	
 /home/pi/HVAC/eReg/_Documents/update.sh
 
-sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005  -Djava.library.path=./  eRegulation.Control
+sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005  -Djava.library.path=./  eRegulation.Control
