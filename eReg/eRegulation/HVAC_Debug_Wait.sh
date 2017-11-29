cbdb767d946b7cc4ba95029abd20f04a20ef700f
Andre <Andre@bapjg.com>
2017-11-29 23:10:07 +0100

    Go Replace _Docs/update by eRegulation/update

--- eReg/eRegulation/HVAC_Debug_Wait.sh
+++ eReg/eRegulation/HVAC_Debug_Wait.sh
@@ -6,6 +6,6 @@ sudo git pull
 
 # sudo chmod 777 *.sh
 	
-/home/pi/HVAC/eReg/_Documents/update.sh
+/home/pi/HVAC/eReg/eRegulation/update.sh
 
 sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005  -Djava.library.path=./  eRegulation.Control
--- eReg/eRegulation/HVAC_Run.sh
+++ eReg/eRegulation/HVAC_Run.sh
@@ -6,7 +6,7 @@ sudo git pull
 
 # sudo chmod 777 *.sh
 	
-/home/pi/HVAC/eReg/_Documents/update.sh
+/home/pi/HVAC/eReg/eRegulation/update.sh
 
 sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
  
