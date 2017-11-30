f440d522ec9266c60c15e13e4ec99f9b997b7f1b
Andre <Andre@bapjg.com>
2017-11-30 22:43:48 +0100

    Autostart

--- eReg/eRegulation/HVAC_AutoStart_Prod.sh
+++ eReg/eRegulation/HVAC_AutoStart_Prod.sh
@@ -1,11 +1,20 @@
 if ! [ -e /home/pi/etc_Setup/autostart_ORI ]; then
+	
+	echo First run : make backups
+	
 	cp /home/pi/.config/lxsession/LXDE-pi/autostart /home/pi/etc_Setup/autostart_ORI
 	cp /home/pi/.config/lxsession/LXDE-pi/autostart /home/pi/etc_Setup/autostart_HVAC
+	
+	echo First run : update autostart_HVAC with extra line
+	
 	echo '@lxterminal --command /home/pi/HVAC/eReg/eRegulation/HVAC_Debug_Wait.sh' >> /home/pi/etc_Setup/autostart_HVAC
 #	echo '@lxterminal --command /home/pi/HVAC/eReg/eRegulation/HVAC_Run.sh'        >> /home/pi/etc_Setup/autostart_HVAC
 fi
 
+echo Copy autostart_HVAC to .config directory
+
 cp -f /home/pi/etc_Setup/autostart_HVAC /home/pi/.config/lxsession/LXDE-pi/autostart
 
+echo Finished
 
 sleep 5
--- eReg/eRegulation/update.sh
+++ eReg/eRegulation/update.sh
@@ -4,4 +4,6 @@ sudo cp -f Bashrc /home/pi/Bashrc
 
 cd   /home/pi/HVAC/eReg/eRegulation
 
-sudo cp -f HVAC_*.sh /home/pi/Desktop
+sudo cp -f HVAC_A*.sh /home/pi/Desktop
+sudo cp -f HVAC_C*.sh /home/pi/Desktop
+sudo cp -f HVAC_R*.sh /home/pi/Desktop
