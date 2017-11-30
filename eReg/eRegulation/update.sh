99bf86d3105d3d1e063f3b7e40fc41cddc192ff9
Andre <Andre@bapjg.com>
2017-11-30 22:50:41 +0100

    dsq

--- eReg/eRegulation/update.sh
+++ eReg/eRegulation/update.sh
@@ -7,3 +7,7 @@ cd   /home/pi/HVAC/eReg/eRegulation
 sudo cp -f HVAC_A*.sh /home/pi/Desktop
 sudo cp -f HVAC_C*.sh /home/pi/Desktop
 sudo cp -f HVAC_R*.sh /home/pi/Desktop
+
+cd /home/pi/Desktop
+rm HVAC_Prod.sh
+rm HVAC_Reset.sh
