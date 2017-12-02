4bdc94fe7c31425092ee9396e76959d709894217
Andre <Andre@bapjg.com>
2017-12-02 10:03:00 +0100

    update

--- eReg/eRegulation/update.sh
+++ eReg/eRegulation/update.sh
@@ -4,9 +4,9 @@ sudo cp -f Bashrc /home/pi/Bashrc
 
 cd   /home/pi/HVAC/eReg/eRegulation
 
-sudo cp -f HVAC_A*.sh /home/pi/Desktop
-sudo cp -f HVAC_C*.sh /home/pi/Desktop
-sudo cp -f HVAC_R*.sh /home/pi/Desktop
+sudo cp -f HVAC_A*.sh     /home/pi/Desktop
+sudo cp -f HVAC_Debug*.sh /home/pi/Desktop
+sudo cp -f HVAC_Run*.sh   /home/pi/Desktop
 
 cd /home/pi/Desktop
 # remove old files if they are still there
