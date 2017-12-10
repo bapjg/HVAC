586534d9dc24c30ca8b7c907401c56f7811ccfd6
Andre <Andre@bapjg.com>
2017-12-10 15:49:45 +0100

    HVAC_Run

--- eReg/eRegulation/HVAC_Run.sh
+++ eReg/eRegulation/HVAC_Run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd /home/pi/HVAC/eReg/eRegulation/
-
-sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
- 
-
 # Status	Description
 #   0       Stop application and go to bash
 #   1       Normal Start/Restart application
@@ -15,13 +10,12 @@ sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Djava.l
 #   6       Debug mode and    wait for debugger to connect
 #   7       Debug mode and NO wait for debugger to connect
 
-
 STATUS=1
 
-# while start/restart/debug (wait or nowait)
-
-while (( STATUS == 1 || STATUS == 6 || STATUS == 7));
+while (( STATUS == 1 || STATUS == 6 || STATUS == 7));		# while start/restart/debug (wait or nowait)
 do
+	cd /home/pi/HVAC/eReg/eRegulation/
+	
 	sudo git stash save
 	sudo git stash drop
 	sudo git pull
@@ -31,23 +25,28 @@ do
 	if [ $STATUS -eq 1 ];	# Normal Restart application
 	then
     	sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Djava.library.path=./            eRegulation.Control
-    	STATUS=$?
+    	NEWSTATUS=$?
 		echo Now restarting
+		sleep 5
     fi
     
     if [ $STATUS -eq 6 ];	# Debug Restart Wait
     then
 		sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005  -Djava.library.path=./  eRegulation.Control
-    	STATUS=$?
+    	NEWSTATUS=$?
 		echo Now Debug/Wait
+		sleep 5
 	fi
 	
 	if [ $STATUS -eq 7 ];	# Debug Restart No Wait
 	then
 		sudo java -cp "../:../../HVAC_Common/bin:javax.mail.jar:gson-2.2.4.jar" -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005  -Djava.library.path=./  eRegulation.Control
-    	STATUS=$?
+    	NEWSTATUS=$?
 		echo Now Debug/NoWait
+		sleep 5
 	fi
+	
+	STATUS=$NEWSTATUS
 	
 done	# EndWhile
 
