72ba7d5560db11a500783cf91a466053bd107dcd
Andre <Andre@AD_Bureau>
2018-09-28 10:01:35 +0200

    Thread_Debug again

--- eReg/eRegulation/Thread_Debug.java
+++ eReg/eRegulation/Thread_Debug.java
@@ -1,15 +1,5 @@
 package eRegulation;
  
-import java.io.EOFException;
-import java.io.File;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Set;
 
 import HVAC_Common.*;
