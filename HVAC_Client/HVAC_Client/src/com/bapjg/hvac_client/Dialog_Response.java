2622df5a916f6e0abf2d0242c747471fd2287f1a
Andre <Andre@AD_Bureau>
2019-04-17 10:28:07 +0200

    HVAC_Client deleted and moved to Android Studio

--- HVAC_Client/HVAC_Client/src/com/bapjg/hvac_client/Dialog_Response.java
+++
@@ -1,9 +0,0 @@
-package com.bapjg.hvac_client;
-
-//------------------------------------------------------------65|-------------------------93|--------------------------------------------------------------------
-public interface Dialog_Response
-{
-	void onDialogReturn			();
-	void onDialogReturnWithId	(int id);
-}
-
--- HVAC_Client/HVAC_Client/src/com/bapjg/hvac_client/TCP_Response.java
+++
@@ -1,9 +0,0 @@
-package com.bapjg.hvac_client;
-
-import HVAC_Common.*;
-
-//------------------------------------------------------------65|-------------------------93|--------------------------------------------------------------------
-public interface TCP_Response 
-{
-	void processFinishTCP(Msg__Abstract output);
-}
